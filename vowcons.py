56290869efd3774a5082455770836e09f503c29d
MeghanaChinnu <madhurichinnu27@gmail.com>
2017-03-15 18:08:54 +0530

    Create vowcons.py

---
+++ vowcons.py
@@ -0,0 +1,8 @@
+vowels = ['a','e','i','o','u']
+l= input('enter the letter:')
+if l in vowels:
+  print("vowel")
+elif l =='y':
+  print("y can either be vowel or consonant")
+else:
+  print("consonant")
