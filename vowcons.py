1d98e1fda32ee8b1f404fb2036b7f999ff132c51
MeghanaChinnu <madhurichinnu27@gmail.com>
2017-03-15 18:13:19 +0530

    Update vowcons.py

--- vowcons.py
+++ vowcons.py
@@ -1,6 +1,6 @@
-vowels = ['a','e','i','o','u']
-l= input('enter the letter:')
-if l in vowels:
+vowelslist = ['a','e','i','o','u']
+l= input('Enter any letter:')
+if l in vowelslist:
   print("vowel")
 elif l =='y':
   print("y can either be vowel or consonant")
