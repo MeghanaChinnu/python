ad318eb3f7514a7e74c8fce5f34c8619c34aa3fb
MeghanaChinnu <madhurichinnu27@gmail.com>
2017-03-16 12:00:40 +0530

    Update largein3.py

--- largein3.py
+++ largein3.py
@@ -1,2 +1,12 @@
-import calendar
-print (calendar.isleap(2016))
+print("Enter the numbers")
+a=input()
+b=input()
+c=input()
+
+if (a>b) and (a>c):
+ num= a
+elif (b>a) and (b>c):
+ num= b
+else:
+ num= c
+print("The largest number is",num)
