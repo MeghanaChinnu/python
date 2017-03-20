5bbe021a505c2df2b3a81504642879b13630992b
MeghanaChinnu <madhurichinnu27@gmail.com>
2017-03-20 20:29:57 +0530

    Update multable.py

--- multable.py
+++ multable.py
@@ -1,5 +1,5 @@
 n = int(input("\n   Enter the number to find the table:"))
 limit = int(input("\n   Enter limit:")) 
 i = 1
-for i in range(1,(limit+1)) :
-    print("\n\t" + str(n) + "*" + str(i)+"="+str((n*i)))
+for i in range(1,(limit+1)):
+    print("\n\t"+str(n)+ "*" + str(i)+"="+str((n*i)))
